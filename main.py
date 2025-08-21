2dacbc2b1f86e490f26e1130cb021bc11ce1d879
cawalamemes <98169617+cawalamemes@users.noreply.github.com>
2025-08-21 17:21:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -59,8 +59,8 @@ s3 = boto3.client(
     "s3",
     region_name="us-east-1",
     endpoint_url="https://objstorage.leapcell.io",
-    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY")
+    aws_access_key_id=os.getenv("ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("SECRET_ACCESS_KEY")
 )
 
 S3_BUCKET = os.getenv("S3_BUCKET")
